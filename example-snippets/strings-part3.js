807af5b36aad3bbc75f3d0d063b368b5617585f2
darzwaitz <darzwaitz@yahoo.com>
2021-07-07 12:05:36 +0100

    added working with strings ex. 3(completed)

--- example-snippets/strings-part3.js
+++ example-snippets/strings-part3.js
@@ -33,3 +33,34 @@ const capitalizeName = function (name) {
 };
 
 capitalizeName(passenger); // Hannah Ann Mary Shmith
+
+// Padding
+const message = 'Go to gate 22';
+// padStart will add the specified character(s), and the 25 here is the specified TOTAL length of the value returned
+console.log(message.padStart(25, '+')); // ++++++++++++Go to gate 22
+console.log('Darz'.padStart(25, '*'));  // *********************Darz
+// padEnd here also specifies TOTAL length of returned value (inc. given value)
+console.log('Darz'.padStart(10, '+').padEnd(20, '-')); // ++++++Darz----------
+
+const maskCreditCard = function (number) {
+    // const str = String() // one way of converting the input to string
+    // this is another technique - by coecing the value to string
+    const str = number + '';
+    const last = str.slice(-4);
+    return last.padStart(str.length, '*');
+}
+
+console.log(maskCreditCard(43215788924)); // *******8924
+console.log(maskCreditCard('43215788924')); // *******8924
+
+// Repeat
+const message2 = 'Bad weather... All departures Delayed...';
+console.log(message2.repeat(4));
+
+const planesInLine = function (n) {
+    console.log(`There are ${n} planes in line ${'✈'.repeat(n)}`);
+}
+
+planesInLine(5); // There are 5 planes in line ✈✈✈✈✈
+planesInLine(2); // There are 2 planes in line ✈✈
+planesInLine(14); //There are 14 planes in line ✈✈✈✈✈✈✈✈✈✈✈✈✈✈
