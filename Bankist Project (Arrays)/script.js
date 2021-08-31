89e13ee7e245dd831cf08936a6d152dcef532fc6
darzwaitz <darzwaitz@yahoo.com>
2021-08-31 17:34:16 +0100

    sort method added - app functionality complete

--- Bankist Project (Arrays)/script.js
+++ Bankist Project (Arrays)/script.js
@@ -61,10 +61,14 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+// show deposits, withdrawals and initiate sort function (sort set to false as default)
+const displayMovements = function (movements, sort = false) {
     containerMovements.innerHTML = '';
 
-    movements.forEach(function (mov, i) {
+    // using slice here, so original array is not mutated
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+    movs.forEach(function (mov, i) {
 
         const type = mov > 0 ? 'deposit' : 'withdrawal';
 
@@ -215,4 +219,14 @@ btnClose.addEventListener('click', function (e) {
     };
     //clear input fields
     inputCloseUsername.value = inputClosePin.value = '';
+});
+
+// track state of sort function
+let sorted = false;
+
+// sort functionality trigger
+btnSort.addEventListener('click', function (e) {
+    e.preventDefault();
+    displayMovements(currentAccount.movements, !sorted);
+    sorted = !sorted;
 });
