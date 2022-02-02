412249b602092ee2be6ceb7fbf641ce0c23b28ab
darzwaitz <darzwaitz@yahoo.com>
2022-02-02 15:53:53 +0000

    Using e.target for page navigation

--- Bankist website/script.js
+++ Bankist website/script.js
@@ -38,15 +38,67 @@ document
     section1.scrollIntoView({ behavior: "smooth" });
   });
 
-const h1 = document.querySelector("h1");
-const alert1 = function (e) {
-  alert("addEventListeny: greysh!");
-};
+// Page navigation
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
 
-h1.addEventListener("mouseenter", alert1);
+// Refactored for better performance
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  // console.log(this);
+  // console.log(e.target);
 
-setTimeout(() => h1.removeEventListener("mouseenter", alert1), 5000);
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+});
+
+// Testing code
+// const h1 = document.querySelector("h1");
+// const alert1 = function (e) {
+//   alert("addEventListeny: greysh!");
+// };
+
+// h1.addEventListener("mouseenter", alert1);
+
+// setTimeout(() => h1.removeEventListener("mouseenter", alert1), 5000);
 
 // h1.onmouseenter = function (e) {
 //   alert("addEventListeny: greysh!");
 // };
+
+// // calc random number
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+
+// const randomColour = () =>
+//   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+// console.log(randomColour(0, 255));
+
+// document.querySelector(".nav__link").addEventListener("click", function (e) {
+//   // this.style.backgroundColor = randomColour();
+//   console.log(`nav LINK`);
+//   e.stopPropagation();
+// });
+
+// document.querySelector(".nav__links").addEventListener("click", function (e) {
+//   // this.style.backgroundColor = randomColour();
+//   console.log(`nav linx`);
+// });
+
+// document.querySelector(".nav").addEventListener("click", function (e) {
+//   // this.style.backgroundColor = randomColour();
+//   console.log(`main nav`);
+// });
---
+++ example-snippets/random-number.js
@@ -0,0 +1,3 @@
+// calc random number
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
