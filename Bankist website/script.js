70ab5a0acd861e01a481b68a45d6127b0d7d4b4a
darzwaitz <darzwaitz@yahoo.com>
2022-02-03 20:36:16 +0000

    UI change on nav links using event handler bound to this

--- Bankist website/script.js
+++ Bankist website/script.js
@@ -7,6 +7,13 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const section1 = document.querySelector("#section--1");
+// Tabbed Component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabContent = document.querySelectorAll(".operations__content");
+
+const nav = document.querySelector(".nav");
 
 const openModal = function () {
   modal.classList.remove("hidden");
@@ -30,8 +37,23 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-// console.log(document.querySelector(".btn--scroll-to"));
-const section1 = document.querySelector("#section--1");
+// nav link hover action
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+// using 'this' bound, as an argument
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
 document
   .querySelector(".btn--scroll-to")
   .addEventListener("click", function () {
@@ -63,11 +85,6 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     });
   }
 });
-
-// Tabbed Component
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabContent = document.querySelectorAll(".operations__content");
 
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".operations__tab");
