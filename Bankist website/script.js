c8c8c0e1b8c7133ebaf8d2f62b5e7a9450e9db16
darzwaitz <darzwaitz@yahoo.com>
2022-02-04 19:42:20 +0000

    reveal section when in view - using Intersection Observer API

--- Bankist website/script.js
+++ Bankist website/script.js
@@ -127,6 +127,28 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+// Reveal sectionz
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(typeof entry);
+  // console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
 // const obsCallback = function (entries, observer) {
 //   entries.forEach((entry) => {
 //     console.log(entry);
