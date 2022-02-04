9ff05069be83a77509a518769663b6160f54387b
darzwaitz <darzwaitz@yahoo.com>
2022-02-04 20:39:21 +0000

    Lazy loading imagez - using Intersection Observer API

--- Bankist website/script.js
+++ Bankist website/script.js
@@ -149,6 +149,33 @@ allSections.forEach(function (section) {
   section.classList.add("section--hidden");
 });
 
+// Lazy loading imagez
+const imgTargets = document.querySelectorAll("img[data-src]");
+// console.log(imgTargets);
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
+
 // const obsCallback = function (entries, observer) {
 //   entries.forEach((entry) => {
 //     console.log(entry);
