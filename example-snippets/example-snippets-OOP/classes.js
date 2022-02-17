8937787fc1347f1ccb960db8f074465fb5e73a46
darzwaitz <darzwaitz@yahoo.com>
2022-02-17 22:31:56 +0000

    added extends for class inheritance in example

--- example-snippets/example-snippets-OOP/classes.js
+++ example-snippets/example-snippets-OOP/classes.js
@@ -29,3 +29,25 @@ console.log(sally.__proto__ === PersonCl.prototype); // true
 //   console.log(`Yo ${this.firstName}`);
 // };
 sally.greet(); // Yo Sally
+
+class StudentCl extends PersonCl {
+  constructor(firstName, birthYear, course) {
+    // parameters from/same as parent
+    super(firstName, birthYear); // from parents class constructor
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name iz ${this.firstName}, studying ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(`I'm ${2037 - this.birthYear + 10}`);
+  }
+}
+
+const martha = new StudentCl("Martha Jonez", 2012, "Computer Science");
+console.log(martha.introduce()); // My name iz Martha Jonez, studying Computer Science
+// console.log(martha.calcAge()); // 25
+
+console.log(martha.calcAge()); // I'm 35
