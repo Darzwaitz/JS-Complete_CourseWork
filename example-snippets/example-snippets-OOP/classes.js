c9c9816d1ab3422b9312155969b727f385163ab7
darzwaitz <darzwaitz@yahoo.com>
2022-02-18 18:10:03 +0000

    Object.create to create prototype chain like a class

--- example-snippets/example-snippets-OOP/classes.js
+++ example-snippets/example-snippets-OOP/classes.js
@@ -51,3 +51,33 @@ console.log(martha.introduce()); // My name iz Martha Jonez, studying Computer S
 // console.log(martha.calcAge()); // 25
 
 console.log(martha.calcAge()); // I'm 35
+
+// creating a class with Object.create()
+
+const PersonProto = {
+  calcAge() {
+    console.log(2030 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+// console.log(steven);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name iz ${this.firstName} etc..`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init("Jay", 2010, "Computer Science");
+jay.introduce(); // My name iz Jay etc..
+jay.calcAge(); // 20
