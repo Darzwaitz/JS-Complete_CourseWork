d098a0c8a19b1869715de2fe7ea8c719c7ea9981
darzwaitz <darzwaitz@yahoo.com>
2022-02-21 23:19:40 +0000

    Another class example

--- example-snippets/example-snippets-OOP/classes.js
+++ example-snippets/example-snippets-OOP/classes.js
@@ -81,3 +81,46 @@ const jay = Object.create(StudentProto);
 jay.init("Jay", 2010, "Computer Science");
 jay.introduce(); // My name iz Jay etc..
 jay.calcAge(); // 20
+
+// another class example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    // console.log("thankz..");
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log("Loan Approved");
+    }
+  }
+}
+
+const acc1 = new Account("Darz", "EUR", 4444);
+
+// acc1.movements.push(240);
+// acc1.movements.push(-40);
+
+acc1.deposit(240);
+acc1.withdraw(70);
+acc1.requestLoan(1000);
+console.log(acc1);
+console.log(acc1.pin);
