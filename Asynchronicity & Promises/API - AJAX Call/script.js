6104964562fd48720a3aee4e49eb4a7d613fc583
darzwaitz <darzwaitz@yahoo.com>
2022-03-04 22:01:21 +0000

    callback within callback

--- Asynchronicity & Promises/API - AJAX Call/script.js
+++ Asynchronicity & Promises/API - AJAX Call/script.js
@@ -4,21 +4,45 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  //   request.open('GET', 'https://restcountries.com/v2/name/ireland');
-  request.send(); // send request to that Url above
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   //   request.open('GET', 'https://restcountries.com/v2/name/ireland');
+//   request.send(); // send request to that Url above
 
-  request.addEventListener('load', function () {
-    //   console.log(typeof this); // object
-    //   console.log(this);
-    //   console.log(this.responseText);
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     //   console.log(typeof this); // object
+//     //   console.log(this);
+//     //   console.log(this.responseText);
+//     const [data] = JSON.parse(this.responseText);
+//     // console.log(data);
 
-    const html = `
-  <article class="country">
+//     const html = `
+//   <article class="country">
+//         <img class="country__img" src="${data.flag}" />
+//         <div class="country__data">
+//         <h3 class="country__name">${data.name}</h3>
+//         <h4 class="country__region">${data.region}</h4>
+//         <p class="country__row"><span>👫</span>${(
+//           +data.population / 1000000
+//         ).toFixed(1)}M people</p>
+//         <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//         <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//         </div>
+//     </article>
+//   `;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('ireland');
+// getCountryData('portugal');
+// getCountryData('germany');
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
         <img class="country__img" src="${data.flag}" />
         <div class="country__data">
         <h3 class="country__name">${data.name}</h3>
@@ -31,11 +55,42 @@ const getCountryData = function (country) {
         </div>
     </article>
   `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // Ajax call - country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  //   request.open('GET', 'https://restcountries.com/v2/name/ireland');
+  request.send(); // send request to that Url above
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // Ajax call - country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send(); // send request to that Url above
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // Render country 2
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('ireland');
-getCountryData('portugal');
-getCountryData('germany');
+// getCountryAndNeighbour('ireland');
+getCountryAndNeighbour('china');
