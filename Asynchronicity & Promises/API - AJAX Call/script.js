7583584c926a808df4f8a7b1c5c0124c881d62c3
darzwaitz <darzwaitz@yahoo.com>
2022-03-09 17:14:31 +0000

    throwing errors manually

--- Asynchronicity & Promises/API - AJAX Call/script.js
+++ Asynchronicity & Promises/API - AJAX Call/script.js
@@ -103,23 +103,70 @@ const renderCountry = function (data, className = '') {
 // Using fetch for the same outcome
 // const request = fetch('https://restcountries.com/v2/name/portugal');
 
+// helper function
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  // the return here is important, as it will link to next then, and needs to return a promise
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
+// Original code before getJSON helper function initiated
+// const getCountryData = function (country) {
+//   // Country 1
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(response => {
+//       console.log(response);
+//       // err => alert(err)
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//       // const neighbour = data[0].borders[0];
+//       const neighbour = 'fghshdfbd';
+
+//       if (!neighbour) return;
+
+//       // Country 2
+//       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+//     })
+//     .then(response => {
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+//       return response.json();
+//     })
+//     .then(data => renderCountry(data, 'neighbour')) // returnd data and class name added
+//     .catch(err => {
+//       console.error(`${err} - message`);
+//       renderError(`Something went wrong - ${err.message}`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
 const getCountryData = function (country) {
   // Country 1
-  fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(
-      response => response.json(),
-      err => alert(err)
-    )
+  getJSON(`https://restcountries.com/v2/name/${country}`, `Country not found`)
     .then(data => {
       renderCountry(data[0]);
       const neighbour = data[0].borders[0];
+      // const neighbour = 'fghshdfbd';
 
-      if (!neighbour) return;
+      // if (!neighbour) return;
+      if (!neighbour) throw new Error('No neighbour found!');
 
       // Country 2
-      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+      return getJSON(
+        `https://restcountries.com/v2/alpha/${neighbour}`,
+        'Country not found'
+      );
     })
-    .then(response => response.json())
     .then(data => renderCountry(data, 'neighbour')) // returnd data and class name added
     .catch(err => {
       console.error(`${err} - message`);
@@ -134,4 +181,8 @@ btn.addEventListener('click', function () {
   getCountryData('portugal');
 });
 
-getCountryData('portugal');
+// getCountryData('portugal');
+// error thrown from first then block with status 404
+// getCountryData('hhhhh'); // Error: Country not found (404) - message
+
+getCountryData('australia');
