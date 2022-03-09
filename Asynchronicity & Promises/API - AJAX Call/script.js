ec92e87e5e7f7e85c118a6bc0db9953da55f53a3
darzwaitz <darzwaitz@yahoo.com>
2022-03-09 14:18:12 +0000

    adding a catch and finally block

--- Asynchronicity & Promises/API - AJAX Call/script.js
+++ Asynchronicity & Promises/API - AJAX Call/script.js
@@ -3,6 +3,30 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}M people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+    </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 // const getCountryData = function (country) {
 //   const request = new XMLHttpRequest();
@@ -39,25 +63,6 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('ireland');
 // getCountryData('portugal');
 // getCountryData('germany');
-
-const renderCountry = function (data, className = '') {
-  const html = `
-  <article class="country ${className}">
-        <img class="country__img" src="${data.flag}" />
-        <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)}M people</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-        </div>
-    </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
 
 // const getCountryAndNeighbour = function (country) {
 //   // Ajax call - country 1
@@ -101,7 +106,10 @@ const renderCountry = function (data, className = '') {
 const getCountryData = function (country) {
   // Country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(response => response.json())
+    .then(
+      response => response.json(),
+      err => alert(err)
+    )
     .then(data => {
       renderCountry(data[0]);
       const neighbour = data[0].borders[0];
@@ -112,7 +120,18 @@ const getCountryData = function (country) {
       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
     })
     .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbour')); // returnd data and class name added
+    .then(data => renderCountry(data, 'neighbour')) // returnd data and class name added
+    .catch(err => {
+      console.error(`${err} - message`);
+      renderError(`Something went wrong - ${err.message}`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
 };
+
+btn.addEventListener('click', function () {
+  getCountryData('portugal');
+});
 
 getCountryData('portugal');
