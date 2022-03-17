27b6c6080bfead3f76c69ff67dabb24895a593cc
darzwaitz <darzwaitz@yahoo.com>
2022-03-17 16:47:00 +0000

    Promisifying the Geolocation app

---
+++ Asynchronicity-Promises/API-AJAX Call/script-2.js
@@ -0,0 +1,7 @@
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+getPosition().then(pos => console.log(pos));
