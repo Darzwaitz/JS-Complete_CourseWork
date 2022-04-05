cdf1b0fe453a1fde6e5cde4d1bd70d5a6832e522
darzwaitz <darzwaitz@yahoo.com>
2022-04-05 18:24:39 +0100

    async await with geoLocation - needs to be fixed

--- Asynchronicity-Promises/API-AJAX Call/script-3.js
+++ Asynchronicity-Promises/API-AJAX Call/script-3.js
@@ -10,10 +10,10 @@ const countriesContainer = document.querySelector('.countries');
 const renderCountry = function (data, className = '') {
   const html = `
   <article class="country ${className}">
-        <img class="country__img" src="${data.flag}" />
+        <img class="country__img" src="${data.countryCode}" />
         <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
+        <h3 class="country__name">${data.countryName}</h3>
+        <h4 class="country__region">${data.continent}</h4>
         <p class="country__row"><span>👫</span>${(
           +data.population / 1000000
         ).toFixed(1)}M people</p>
@@ -26,43 +26,32 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-// getPosition().then(pos => console.log(pos));
-
-// const whereAmI = function () {
-//   getPosition()
-//     .then(pos => {
-//       const { latitude: lat, longitude: lng } = pos.coords;
-
-//       //   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-//       return fetch(
-//         `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
-//       );
-//     })
-//     .then(res => {
-//       if (!res.ok) throw new Error(`Problem with geocoding...`);
-//       return res.json();
-//     })
-//     .then(data => {
-//       console.log(data);
-//       console.log(`You are in ${data.city}, ${data.countryName}`);
-
-//       return fetch(`https://restcountries.com/v2/name/${data.countryName}`);
-//     })
-//     .then(response => {
-//       if (!response.ok) throw new Error('Country not found');
-//       return response.json();
-//     })
-//     .then(data => renderCountry(data[0]))
-//     .catch(err => console.error(`${err.message}`))
-//     .finally(() => {
-//       countriesContainer.style.opacity = 1;
-//     });
-// };
-
-btn.addEventListener('click', whereAmI);
-
-const whereAmI = async function (country) {
-  await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
-  );
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
 };
+
+const whereAmI = async function () {
+  const pos = await getPosition();
+  const { latitude: lat, longitude: lng } = pos.coords;
+
+  const resGeo = await fetch(
+    // `https://restcountries.com/v2/${lat},${lng}?geoit=json`
+
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}?geoit=json`
+  );
+  const dataGeo = await resGeo.json();
+  console.log(dataGeo);
+
+  const res = await fetch(
+    // `https://restcountries.com/v2/name/${dataGeo.countryName}`
+    `https://restcountries.eu/rest/v2/name/${dataGeo.countryName}`
+  );
+  // console.log(`RES: ${res}`);
+  const data = await res.json();
+  // console.log(`DATA: ${data}`);
+  renderCountry(data[0]);
+};
+
+whereAmI();
