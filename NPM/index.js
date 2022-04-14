f7f8022a277df97e5e085f635d55fccd08a3ec2d
darzwaitz <darzwaitz@yahoo.com>
2022-04-14 17:53:18 +0100

    Lodash example in NPM

---
+++ NPM/index.js
@@ -0,0 +1,20 @@
+// Using cloneDeep from loadash to make sure a clone object is the original object
+
+import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+
+const state = {
+  cart: [
+    { product: "bread", quantity: 5 },
+    { product: "pizza", quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+console.log(stateClone);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
