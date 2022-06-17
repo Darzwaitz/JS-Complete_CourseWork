9c806f844cb3fbe13a42b772e8210b300e34053c
darzwaitz <darzwaitz@yahoo.com>
2022-06-17 16:04:52 +0100

    Code refactor example 1 - Section 17 - Part 2 refactor

--- NPM/code refactor - example 1/clean.js
+++ NPM/code refactor - example 1/clean.js
@@ -14,6 +14,8 @@ const spendingLimits = {
   matilda: 100,
 };
 
+const getLimit = user => spendingLimits?.[user] ?? 0;
+
 const addExpense = function (value, description, user = 'jonas') {
   // if (!user) user = 'jonas';
   user = user.toLowerCase();
@@ -25,10 +27,14 @@ const addExpense = function (value, description, user = 'jonas') {
   //   lim = 0;
   // }
 
-  const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+  // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+  // using optional chaining - if this property existz set it as that - and if not  to set is as 0
+  // const limit = spendingLimits?.[user] ?? 0;
 
-  if (value <= limit) {
-    budget.push({ value: -value, description: description, user: user });
+  // const limit = getLimit(user);
+
+  if (value <= getLimit(user)) {
+    budget.push({ value: -value, description, user });
   }
 };
 addExpense(10, 'Pizza 🍕');
@@ -36,32 +42,30 @@ addExpense(100, 'Going to movies 🍿', 'Matilda');
 addExpense(200, 'Stuff', 'Jay');
 console.log(budget);
 
-const check = function () {
-  for (const el of budget) {
-    let lim;
-    if (spendingLimits[el.user]) {
-      lim = spendingLimits[el.user];
-    } else {
-      lim = 0;
-    }
+const checkExpenses = function () {
+  // let lim;
+  // if (spendingLimits[entry.user]) {
+  //   lim = spendingLimits[entry.user];
+  // } else {
+  //   lim = 0;
+  // }
 
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
-  }
+  // const limit = spendingLimits?.[entry.user] ?? 0;
+
+  for (const entry of budget)
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
 };
-check();
+checkExpenses();
 
 console.log(budget);
 
-const bigExpenses = function (limit) {
+const logBigExpenses = function (bigLimit) {
   let output = '';
-  for (const el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
-  }
+  for (const entry of budget)
+    output +=
+      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
+
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
-bigExpenses(1000);
+logBigExpenses(1000);
